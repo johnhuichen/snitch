6205fc613e7cc3cfa0e095de27f544bd42ef39a0
John Chen <johnhuichen@gmail.com>
2023-09-06 19:40:51 -0400

    Created script to grab hostname of local tcp connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use dns_lookup;
+use procfs::net::tcp;
+use std::{collections, net};
+
 fn main() {
-    println!("Hello, world!");
+    let ignore_ips = ["0.0.0.0", "127.0.0.1"];
+
+    let hosts = tcp()
+        .unwrap()
+        .into_iter()
+        .filter_map(|entry| {
+            let ip = entry.remote_address.ip().to_string();
+            if ignore_ips.contains(&ip.as_str()) {
+                return None;
+            }
+            let ip: net::IpAddr = ip.parse().unwrap();
+            dns_lookup::lookup_addr(&ip).ok()
+        })
+        .collect::<collections::HashSet<_>>();
+
+    println!("{:?}", hosts);
 }
